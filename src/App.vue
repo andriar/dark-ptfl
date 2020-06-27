b0aa5569914b28ac10fefe086dc6ef607fa09faf
andriar <andriar.fazzan@gmail.com>
2020-06-27 13:57:05 +0700

    added meta

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,25 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link>|
-      <router-link to="/about">About</router-link>
+      <!-- <router-link to="/">Home</router-link>|
+      <router-link to="/about">About</router-link> -->
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  metaInfo: {
+    title: "Andriar Mafazan Rizkiansah",
+    titleTemplate: "%s - Porofolio",
+    htmlAttrs: {
+      lang: "en",
+      amp: true,
+    },
+  },
+};
+</script>
 
 <style lang="scss">
 body {
--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,19 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
+import VueMeta from "vue-meta";
 
 Vue.config.productionTip = false;
 
+Vue.use(VueMeta, {
+  keyName: "metaInfo",
+  attribute: "data-vue-meta",
+  ssrAttribute: "data-vue-meta-server-rendered",
+  tagIDKeyName: "vmid",
+  refreshOnceOnNavigation: true,
+});
 new Vue({
   router,
   store,
-  render: h => h(App)
+  render: (h) => h(App),
 }).$mount("#app");
